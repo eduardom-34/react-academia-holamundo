f3ba373b48c561db3338a30629d7003487eeadb7
CesarLinux <eduardomelchor34@gmail.com>
2025-06-08 20:24:17 -0600

    Gestion de estado

--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -1,3 +1,4 @@
+// import type { MouseEvent } from "react";
 
 type Props = {
   data: string[]
@@ -5,13 +6,16 @@ type Props = {
 
 export const List = (props: Props) => {
 
+  const handleClick = ( e: string ) => {
+    console.log(e)
+  }
+
   const { data } = props;
 
   return (
     <ul className="list-group">
       {data.map(elemento => (
-        <li key={elemento} className="list-group-item">{ elemento }</li>
-
+        <li onClick={ () => handleClick(elemento) } key={elemento} className="list-group-item">{elemento}</li>
       ))}
     </ul>
   )
